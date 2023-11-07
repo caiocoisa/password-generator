f40a2987615e193bf2f8fa54d39818a8cc0c878a
Caio Silva <silvacaiodev@gmail.com>
2023-11-07 16:01:20 -0300

    chore: Remove unused code

--- password-generator/src/app/page.tsx
+++ password-generator/src/app/page.tsx
@@ -2,12 +2,8 @@
 
 import styles from "./page.module.css";
 import PasswordInput from "./components/PasswordInput";
-import StrengthBar from "./components/StrengthBar";
-import { StrengthType } from "./_types/StrengthTypes";
 import FilterPanel from "./components/FilterPanel";
-import { useEffect, useState } from "react";
 import { FilterContextProvider } from "./context/filter-context";
-import useFilter from "./hooks/useFilter";
 
 export default function Home() {
 
